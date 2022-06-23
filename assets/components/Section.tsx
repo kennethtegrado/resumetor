05831c47bc414cd621ccfef21c0c196414067dbf
Kenneth Tegrado <tegradok@gmail.com>
2022-06-23 16:03:31 +0800

    feat: add shadow color and div for containing inputs on the header

--- assets/components/Section.tsx
+++ assets/components/Section.tsx
@@ -3,7 +3,9 @@ import type { FunctionComponent } from 'react';
 const Section: FunctionComponent<SectionProps> = ({ title }) => {
     return (
         <section className="section__container">
-            <h4 className="section__title">{title}</h4>
+            <div>
+                <h4 className="section__title">{title}</h4>
+            </div>
         </section>
     );
 };

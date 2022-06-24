5a9ae7d2ac700f4cd6e46289216b89d598556760
Kenneth Tegrado <tegradok@gmail.com>
2022-06-24 13:35:50 +0800

    feat: create ViewData component

---
+++ assets/components/ui/ViewData.tsx
@@ -0,0 +1,11 @@
+import type { FunctionComponent } from 'react';
+
+const ViewData: FunctionComponent<ViewDataProps> = ({ data }) => {
+    return <div></div>;
+};
+
+export default ViewData;
+
+interface ViewDataProps {
+    data: any;
+}

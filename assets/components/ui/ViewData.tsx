88194237dd6dd3bcc3a566a981777d369b562b8b
Kenneth Tegrado <tegradok@gmail.com>
2022-06-24 15:45:33 +0800

    feat: fix tsconfig and added content for viewdata

--- assets/components/ui/ViewData.tsx
+++ assets/components/ui/ViewData.tsx
@@ -1,11 +1,36 @@
-import type { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 
+import { makeArray } from '@utils';
 const ViewData: FunctionComponent<ViewDataProps> = ({ data }) => {
-    return <div></div>;
+    const [fields, setFields] = useState<Fields | any>([]);
+
+    // Use Effect
+    useEffect(() => {
+        (async () => {
+            const dataArray = await makeArray(data);
+            setFields(dataArray);
+        })();
+    }, [data]);
+
+    return (
+        <div>
+            {fields.length &&
+                fields.map((item: Fields, index: number) => {
+                    <div key={index}>
+                        <h6>{item.key}</h6>;<p>{item.value}</p>;
+                    </div>;
+                })}
+        </div>
+    );
 };
 
 export default ViewData;
 
 interface ViewDataProps {
     data: any;
+}
+
+interface Fields {
+    key: string;
+    value: string;
 }

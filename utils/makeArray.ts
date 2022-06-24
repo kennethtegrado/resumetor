98956102e4d854d4b2a63c726abb1955e3a2d0c9
Kenneth Tegrado <tegradok@gmail.com>
2022-06-24 14:12:28 +0800

    feat: change makeArray configurations

--- utils/makeArray.ts
+++ utils/makeArray.ts
@@ -1,23 +1,37 @@
-const makeArray = ({ data }: makeArrayParams): Array<any> => {
+const makeArray = (data: any): Array<makeArrayResult | any> => {
     const dataArray = [];
 
     for (const key in data) {
         let dataItem: dataItem = {};
-        dataItem.key = key;
-        dataItem.value = data.key;
-        dataArray.push(dataItem);
+
+        if (!Array.isArray(data[key])) {
+            dataItem.key = key;
+            dataItem.value = data[key];
+            dataArray.push(dataItem);
+        } else {
+            data[key].forEach((item: any, index: number) => {
+                const nestedKeys = Object.keys(item);
+
+                for (const nestedKey of nestedKeys) {
+                    dataItem.key = `${nestedKey} ${index + 1}`;
+                    dataItem.value = data[key][index][nestedKey];
+                    dataArray.push(dataItem);
+                }
+            });
+        }
     }
 
-    return [];
+    return dataArray;
 };
 
 export default makeArray;
 
-interface makeArrayParams {
-    data: any;
-}
-
 interface dataItem {
     key?: string;
     value?: any;
+}
+
+interface makeArrayResult {
+    key: string;
+    value: string;
 }

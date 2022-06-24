218e361bf20e86a51a71e9f4dab5fdf0cbe2e4b1
Kenneth Tegrado <tegradok@gmail.com>
2022-06-24 17:02:34 +0800

    feat: design the content for the view data

--- assets/components/ui/ViewData.tsx
+++ assets/components/ui/ViewData.tsx
@@ -2,14 +2,14 @@ import { FunctionComponent, useEffect, useState } from 'react';
 
 const ViewData: FunctionComponent<ViewDataProps> = ({ data }) => {
     return (
-        <>
+        <div className="viewdata">
             {data.map(({ key, value }: Fields, index: number) => (
-                <div key={index}>
-                    <h6>{key}</h6>
-                    <p className="text__body-2">{value}</p>
+                <div key={index} className="viewdata__content">
+                    <h6 className="viewdata__title">{key}</h6>
+                    <p className="viewdata__text text__body-2">{value}</p>
                 </div>
             ))}
-        </>
+        </div>
     );
 };
 

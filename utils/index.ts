6adb86efffe8cae6af2e22cfc426e0c182850225
Kenneth Tegrado <tegradok@gmail.com>
2022-07-03 16:40:45 +0800

    refactor: add import for utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
 import makeArrayHeaderData from './makeArrayHeaderData';
 import shortenLink from './shortenLink';
+import sortArrayEducationData from './sortEducationData';
 
-export { makeArrayHeaderData, shortenLink };
+export { makeArrayHeaderData, shortenLink, sortArrayEducationData };
--- utils/sortEducationData.ts
+++ utils/sortEducationData.ts
@@ -1,15 +1,20 @@
-import type { FieldValues } from 'react-hook-form';
+import type { Field, FieldValues } from 'react-hook-form';
 
 /**
  * Sort the array of objects by the startYear property in ascending order.
- * @param {FieldValues[]} data - FieldValues[]
+ * @param {FieldValues} data - FieldValues[]
  */
-const sortArrayEducationData = async (data: FieldValues[]): Promise<void> => {
-    data.sort((firstItem, secondItem) => {
-        if (+firstItem.startYear > +secondItem.startYear) return -1;
-        else if (+firstItem.startYear < +secondItem.startYear) return 1;
-        else return 0;
-    });
+const sortArrayEducationData = async (data: FieldValues): Promise<void> => {
+    data.sort(
+        (
+            firstItem: { startYear: string },
+            secondItem: { startYear: string }
+        ) => {
+            if (+firstItem.startYear > +secondItem.startYear) return -1;
+            else if (+firstItem.startYear < +secondItem.startYear) return 1;
+            else return 0;
+        }
+    );
 };
 
 export default sortArrayEducationData;

cdea10a49c6645bdbbac0f2454bd48d9cc6d78b6
Kenneth Tegrado <tegradok@gmail.com>
2022-07-08 21:22:01 +0800

    feat: add new section declaration types

--- interface/sections.ts
+++ interface/sections.ts
@@ -1,1 +1,10 @@
+import type { FunctionComponent } from 'react';
+import type { SubmitHandler, FieldValues } from 'react-hook-form';
+
 export type SectionTypes = 'education' | 'header' | 'experience' | 'skill';
+export interface FormComponentProps {
+    submitHandler: SubmitHandler<FieldValues>;
+    viewSectionContent: boolean;
+}
+
+export type FormComponent = FunctionComponent<FormComponentProps>;
